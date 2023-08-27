f9a2f83b538b3589098455528028e7ea44225628
Lekematti <leo.koskimaki@gmail.com>
2023-08-27 13:16:58 +0300

    style valmis

--- wbma-example/App.js
+++ wbma-example/App.js
@@ -2,8 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import List from './components/List';
 import {SafeAreaView,} from 'react-native';
 
-
-
 const App = () => {
   return (
     <>
--- wbma-example/components/ListItem.js
+++ wbma-example/components/ListItem.js
@@ -7,11 +7,9 @@ const ListItem = ({singleMedia}) => {
     }}>
       <View style={styles.container}>
         <Image
-          style={{width: 100, height: 100}}
+          style={{borderRadius: 10,width: 100, height: 100}}
           source={{uri: singleMedia.thumbnails.w160}}
         />
-      </View>
-      <View style={styles.container}>
         <Text style={styles.text}>{singleMedia.title}</Text>
         <Text style={styles.text}>{singleMedia.description}</Text>
       </View>
@@ -21,14 +19,23 @@ const ListItem = ({singleMedia}) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#000',
+    flexWrap: 'wrap',
+    flexGrow: 1,
+    marginTop: 30,
+    padding: 10,
+    backgroundColor: '#fff',
     alignItems: 'center',
+    alignContent: 'center',
     justifyContent: 'center',
   },
   text: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexWrap: 'wrap',
+    flexGrow: 1,
+    margin: 5,
+    padding: 5,
+    borderRadius: 10,
+    backgroundColor: '#000',
+    color: '#4CB7FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
