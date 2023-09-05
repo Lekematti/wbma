9cbb2632392b64215c49b76252f17af4038e575b
Lekematti <leo.koskimaki@gmail.com>
2023-09-05 15:33:38 +0300

    comb-lib

--- wbma-example/components/ListItem.js
+++ wbma-example/components/ListItem.js
@@ -1,51 +1,29 @@
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
 import PropTypes from 'prop-types';
 import {mediaUrl} from '../utils/app-config';
+import {Avatar, Card, ListItem as RNEListItem} from '@rneui/themed';
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
 
 const ListItem = ({singleMedia, navigation}) => {
   return (
+    <Card>
     <TouchableOpacity
       onPress={() => {
         console.log('touched!', singleMedia.title);
         navigation.navigate('Single', singleMedia);
       }}
     >
-      <View style={styles.container}>
-        <Image
-          style={{width: 100, height: 100, borderRadius: 20}}
-          source={{uri: mediaUrl + singleMedia.thumbnails.w160}}
-        />
-        <Text style={styles.text}>{singleMedia.title}</Text>
-        <Text style={styles.text}>{singleMedia.description}</Text>
-      </View>
+      <RNEListItem>
+        <Avatar size="xlarge" source={{uri: mediaUrl + singleMedia.thumbnails.w160}}/>
+      </RNEListItem>
+
+      <RNEListItem.Title>{singleMedia.title}</RNEListItem.Title>
+      <RNEListItem.Subtitle>{singleMedia.description}</RNEListItem.Subtitle>
     </TouchableOpacity>
+    </Card>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    borderRadius: 20,
-    flexWrap: 'wrap',
-    flexGrow: 1,
-    marginTop: 30,
-    padding: 10,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    alignContent: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    flexWrap: 'wrap',
-    flexGrow: 1,
-    margin: 5,
-    padding: 5,
-    borderRadius: 10,
-    backgroundColor: '#a5a5a5',
-    color: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 ListItem.propTypes = {
   singleMedia: PropTypes.object,
